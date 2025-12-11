6262cf3499c780ab438c0efc7affd658ebb0395c
nancy-shehata <159195246+nancy-shehata@users.noreply.github.com>
2025-12-11 12:33:11 -0500

    updated UI

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let gridSize = 16;
 const gridSizeSlider = document.getElementById("myRange");
 const displayGridVal = document.querySelector(".display-grid-size-text");
 gridSizeSlider.addEventListener("input", ()=>{
-        displayGridVal.textContent = gridSizeSlider.value + " x " + gridSizeSlider.value;
+        displayGridVal.textContent = "Grid Size: " + gridSizeSlider.value + " x " + gridSizeSlider.value;
         gridContainer.textContent = "";
         createGrid(gridSizeSlider.value);
 
@@ -17,7 +17,7 @@ const colorPicker = document.querySelector(".color-picker");
 
 
 function createGrid(size){
-    const cellSize = 400/size;
+    const cellSize = 600/size;
     console.log(gridSizeSlider.value)
     for (let i=0;i<size; i++){      //row
         for (let j=0; j<size; j++){     //column 
@@ -41,3 +41,19 @@ function createGrid(size){
 
 }
 createGrid(gridSize);
+
+
+function clearGrid(){
+    const clear = document.querySelector(".clear-btn");
+    const gridCell = document.querySelectorAll(".grid-cell")
+        gridCell.forEach((gridCell) =>{
+            clear.addEventListener("click", () =>{
+                gridCell.style.backgroundColor = "";
+                console.log("clicked")
+                
+            });
+
+        });
+
+}
+clearGrid();
