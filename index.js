184babc2d52713f30d16a20a642d891ae277753c
nancy-shehata <159195246+nancy-shehata@users.noreply.github.com>
2025-12-12 10:40:42 -0500

    adding clouds

--- index.js
+++ index.js
@@ -58,3 +58,22 @@ function clearGrid(){
 
 }
 clearGrid();
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const sky = document.getElementById("sky")
+    const cloudNum = 25;
+
+    function createCloud(){
+        const cloud = document.createElement("img")
+        const randomSize = Math.random() * 400 + 800;
+        cloud.style.width = randomSize + "px";
+        cloud.src = "images/cloud.png"
+        sky.appendChild(cloud)
+        cloud.classList.add("cloud");
+    }
+    for(let i=0; i<cloudNum;i++){   
+    createCloud();
+    }
+})
